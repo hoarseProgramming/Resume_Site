1abec34b8a7849498c93793b803d68397fe755a4
Hampus Eiderström Swahn <hampus.es@gmail.com>
2025-02-06 22:05:37 +0100

    A lot of different fixes, added CV page

---
+++ CV.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const toggler = document.getElementById("toggler");
+    const pdf = document.getElementById("pdf");
+    let isSwedishPdf = true;
+    
+    toggler.addEventListener("click", () => {
+        if (isSwedishPdf){
+            pdf.src = "Media/Images/CVEN.pdf";
+            isSwedishPdf = !isSwedishPdf;
+        }
+        else {
+            pdf.src = "Media/Images/CVSE.pdf";
+            isSwedishPdf = !isSwedishPdf;
+        }
+    })
+});
